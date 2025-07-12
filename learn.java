d6630ff758b45fadde646f5b4da9fbbec9407023
ShreeRamVaishnav <samratking120@gmial.com>
2025-07-12 14:47:06 +0530

    new changes

--- learn.java
+++ learn.java
@@ -6,11 +6,13 @@ public class learn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = 0;
-
         for (int i = 1; i <= n; i++) {
-            sum = sum + i;
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+
         }
-        System.out.println("sum of n number is :" + sum);
     }
 }
--- learn.js
+++ learn.js
@@ -1,6 +1,21 @@
-let n = prompt("enter  first number");
-let f = 1;
-for (let i = 1; i <= 10; i++) {
-  f = f * i;
+let a = prompt("enter a number");
+let operator = prompt("enter a operator");
+let b = prompt("enter a number");
+
+switch (operator) {
+  case "+":
+    console.log(`sum of : ${a}+${b}=${Number(a) + Number(b)}`);
+    break;
+  case "+":
+    console.log(`substraction of : ${a}-${b}=${Number(a) - Number(b)}`);
+    break;
+  case "+":
+    console.log(`multipication of : ${a}*${b}=${Number(a) * Number(b)}`);
+    break;
+  case "+":
+    console.log(`division of : ${a}/${b}=${Number(a) / Number(b)}`);
+    break;
+
+  default:
+    break;
 }
-console.log("factorial is :" + f);
