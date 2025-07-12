2bb1081565f8b27c3cfed6057befaba27dd82842
ShreeRamVaishnav <samratking120@gmial.com>
2025-07-13 00:48:28 +0530

    new_changes

--- learn.js
+++ learn.js
@@ -1,13 +1,28 @@
-let n = prompt("enter a number");
-let lastdigit;
-let rev = 0;
-// for (let i = 1; i <= n; i++) {
-//   f = f * i;
+//For even number
+// let n = prompt("enter a number");
+
+// for (let i = 16; i >= n; i = i - 2) {
+//   console.log(i);
 // }
-// console.log("factorial is :" + f);
-while (n > 0) {
-  lastdigit = n % 10;
-  // rev = rev * 10 + lastdigit;
-  console.log("rev is :" + lastdigit);
-  n = n / 10;
+
+// for odd number
+// let n = prompt("enter a number");
+// for (let i = 1; i <= n; i = i + 2) {
+//   console.log(i);
+// }
+
+//sum of n number
+// let n = prompt("enter number");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+// }
+// console.log("sum is :" + sum);
+
+//print multipication table
+let n = prompt("enter a number");
+n = parseInt(n);
+
+for (let i = n; i <= n * 10; i = i + n) {
+  console.log(i);
 }
