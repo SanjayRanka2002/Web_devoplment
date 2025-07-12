062958d8f49528280ecb7ddfd33d9bbc223d9352
ShreeRamVaishnav <samratking120@gmial.com>
2025-07-12 12:38:11 +0530

    leaning

---
+++ learn.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class learn {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i + j <= n + 1) {
+                    System.out.print(i);
+
+                }
+
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
---
+++ learn.js
@@ -0,0 +1,6 @@
+let n = prompt("enter  first number");
+let f = 1;
+for (let i = 1; i <= 10; i++) {
+  f = f * i;
+}
+console.log("factorial is :" + f);
