96ba13c42c43cefed361384c60613d5ab7a97fdf
ShreeRamVaishnav <samratking120@gmial.com>
2025-07-13 16:51:33 +0530

    new-code-add

--- learn.java
+++ learn.java
@@ -5,22 +5,64 @@ public class learn {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //print sum of n number
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     sum = sum + i;
+        // }
+        // System.out.println("sum of n number is :" + sum);
+
+        //print factorial of a number
+        // int n = sc.nextInt();
+        // int f = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     f = f * i;
+        // }
+        // System.out.println("factorial is :" + f);
+        //print a reverse of input user number
+        // int n = sc.nextInt();
+        // int lastdigit;
+        // while (n > 0) {
+        //     lastdigit = n % 10;
+        //     System.out.print(lastdigit);
+        //     n = n / 10;
+        // }
+        //reverse by given number 
+        // int n = sc.nextInt();
+        // int lastdigit;
+        // int rev = 0;
+        // while (n > 0) {
+        //     lastdigit = n % 10;
+        //     rev = (rev * 10) + lastdigit;
+        //     System.out.println("rev is :" + rev);
+        //     n = n / 10;
+        // }
+        //Basic patterns square
         // int n = sc.nextInt();
         // for (int i = 1; i <= n; i++) {
         //     for (int j = 1; j <= n; j++) {
         //         System.out.print("*");
-
+        //     }
+        //     System.out.println(" ");
+        // }
+        //holo pattern
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+        //holo reverse
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (i + j <= n + 1) {
+        //             System.out.print("*");
+        //         }
         //     }
         //     System.out.println("");
         // }
-        int n = sc.nextInt();
-        int lastdigit;
-        while (n > 0) {
-            lastdigit = n % 10;
-            System.out.print(lastdigit);
-            n = n / 10;
-
-        }
-
     }
 }
