4b02e6f2105671ee1a28bbf0c7ce9b8338c38434
ShreeRamVaishnav <samratking120@gmial.com>
2025-07-13 22:18:23 +0530

    new_add

--- learn.java
+++ learn.java
@@ -5,7 +5,7 @@ public class learn {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //print sum of n number
+        // print sum of n number
         // int n = sc.nextInt();
         // int sum = 0;
         // for (int i = 1; i <= n; i++) {
@@ -13,14 +13,14 @@ public class learn {
         // }
         // System.out.println("sum of n number is :" + sum);
 
-        //print factorial of a number
+        // print factorial of a number
         // int n = sc.nextInt();
         // int f = 1;
         // for (int i = 1; i <= n; i++) {
         //     f = f * i;
         // }
         // System.out.println("factorial is :" + f);
-        //print a reverse of input user number
+        // print a reverse of input user number
         // int n = sc.nextInt();
         // int lastdigit;
         // while (n > 0) {
@@ -28,7 +28,7 @@ public class learn {
         //     System.out.print(lastdigit);
         //     n = n / 10;
         // }
-        //reverse by given number 
+        // reverse by given number 
         // int n = sc.nextInt();
         // int lastdigit;
         // int rev = 0;
@@ -38,7 +38,7 @@ public class learn {
         //     System.out.println("rev is :" + rev);
         //     n = n / 10;
         // }
-        //Basic patterns square
+        // Basic patterns square
         // int n = sc.nextInt();
         // for (int i = 1; i <= n; i++) {
         //     for (int j = 1; j <= n; j++) {
@@ -46,7 +46,7 @@ public class learn {
         //     }
         //     System.out.println(" ");
         // }
-        //holo pattern
+        // star pattern
         // int n = sc.nextInt();
         // for (int i = 1; i <= n; i++) {
         //     for (int j = 1; j <= i; j++) {
@@ -54,7 +54,7 @@ public class learn {
         //     }
         //     System.out.println(" ");
         // }
-        //holo reverse
+        // holo reverse
         // int n = sc.nextInt();
         // for (int i = 1; i <= n; i++) {
         //     for (int j = 1; j <= n; j++) {
@@ -64,5 +64,36 @@ public class learn {
         //     }
         //     System.out.println("");
         // }
+        // int n = sc.nextInt(); // Ensure Scanner object is initialized before this line
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         if (i + j <= n + 1) {
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println("");
+        // }
+        // 1,12,123,1234 pattern wala 
+        // int n = sc.nextInt();
+        // int count = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+        // pattern of charcter like A,BC,CDE,FGHI
+        int n = sc.nextInt();
+        char c = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c);
+                c++;
+
+            }
+            System.out.println("");
+
+        }
+
     }
 }
