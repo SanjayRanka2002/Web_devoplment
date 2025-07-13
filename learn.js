039e46a7d692e665f64088e67d168edb1d2c4097
ShreeRamVaishnav <samratking120@gmial.com>
2025-07-13 12:15:14 +0530

    Mesage_learn

--- learn.js
+++ learn.js
@@ -20,9 +20,48 @@
 // console.log("sum is :" + sum);
 
 //print multipication table
-let n = prompt("enter a number");
-n = parseInt(n);
+// let n = prompt("enter a number");
+// n = parseInt(n);
 
-for (let i = n; i <= n * 10; i = i + n) {
-  console.log(i);
-}
+// for (let i = n; i <= n * 10; i = i + n) {
+//   console.log(i);
+// }
+
+//for square pattern
+// let n = 4;
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     console.log(j);
+//   }
+// }
+
+//reverse number series
+// let i = 5;
+// while (i >= 1) {
+//   console.log(i);
+//   i--;
+// }
+
+//print multiple of 3
+// print multiple of n
+// let n = prompt("enter number");
+// n = parseInt(n);
+// let i = n;
+// while (i <= n * 10) {
+//   console.log(i);
+//   i = i + n;
+// }
+
+//guess college name game
+
+// let college = "lachoo";
+// let guess = prompt("enter college name");
+
+// while (guess != college && guess != "quit") {
+//   guess = prompt("try again");
+// }
+// if (guess == college) {
+//   console.log("congratulation you guess right college");
+// } else {
+//   console.log("you quit this game");
+// }
